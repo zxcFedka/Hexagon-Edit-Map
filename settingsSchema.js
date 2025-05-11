537290fae33beafe87f3487ec06e22f146f25ac7
zxcFedka <barsuk20367@gmail.com>
2025-05-11 11:13:41 +0400

    update

--- settingsSchema.js
+++ settingsSchema.js
@@ -3,7 +3,9 @@ const settingsSchema = [
     { name: "Title", type: "string", defaultValue: "New Hex", luaName: "Title" },
     { name: "Description", type: "string", defaultValue: "", luaName: "Description" },
     { name: "Cost", type: "number", defaultValue: 0, luaName: "Cost" },
-    { name: "visible", type: "boolean", defaultValue: true, luaName: "visible" }
+    { name: "visible", type: "boolean", defaultValue: true, luaName: "visible" },
+    { name: "Previous Map", type: "boolean", defaultValue: true, luaName: "PreviousMap" },
+    { name: "Next Map", type: "boolean", defaultValue: true, luaName: "NextMap" }
     // NextHex здесь не указываем, так как он управляется интерактивно
     // и его структура (массив строк) отличается от простых полей
 ];
